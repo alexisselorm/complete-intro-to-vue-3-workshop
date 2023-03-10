b9ef71009bd02ef40c1ad18b1ccfecbc4fcde596
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-03-11 01:43:25 +0200

    Update UsersPage.vue

--- cest-la-vue/src/components/UsersPage.vue
+++ cest-la-vue/src/components/UsersPage.vue
@@ -1,21 +1,32 @@
 <template>
-    <p v-for="user in users" :key="user.id">{{ user.name }}</p>
+  <p class="user-card" v-for="user in users" :key="user.id">
+    {{ user.name }} - {{ user.website }}
+  </p>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        users: [],
-    }),
-    methods: {
-        async fetchUsers() {
-            this.users = await fetch(
-                "https://jsonplaceholder.typicode.com/users"
-            ).then((response) => response.json());
-        },
+  data: () => ({
+    users: [],
+  }),
+  methods: {
+    async fetchUsers() {
+      this.users = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((response) => response.json());
     },
-    created() {
-        this.fetchUsers();
-    },
+  },
+  created() {
+    this.fetchUsers();
+  },
 };
 </script>
+
+<style>
+.user-card {
+  border: 1px solid #222;
+  max-width: 400px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
